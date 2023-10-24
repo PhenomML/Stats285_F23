ab8c4134c902cb9378a2308567607d99def1b4af
adonoho <github@store.ddg.com>
2023-10-24 16:25:42 -0500

    Test without the module load

--- hw2.sh
+++ hw2.sh
@@ -9,8 +9,8 @@
 #SBATCH --output=hw2.out
 
 ## Load the python module
-module load anaconda3/2023.07
-source activate stats285
+# module load anaconda3/2023.07
+# source activate stats285
 
 ## Run the python script
 time python3 ~/Stats285_hw3/main.py
