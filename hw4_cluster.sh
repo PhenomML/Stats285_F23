64e2b0f4ffeba5a968dd2a94ea9c3011428444c8
adonoho <github@store.ddg.com>
2023-10-29 18:06:25 -0500

    Add memory designation.

--- hw4_cluster.sh
+++ hw4_cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## farmshare deployment file that requests 8 cpu cores for 30 minutes to run main.py
-#SBATCH --job-name=hw4
+#SBATCH --job-name=hw4_cluster
 #SBATCH --partition=normal
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=4
 #SBATCH --time=00:30:00
-#SBATCH --error=hw4.err
-#SBATCH --output=hw4.out
+#SBATCH --error=hw4_cluster.err
+#SBATCH --output=hw4_cluster.out
 
 ## Run the python script
 time python3 ~/Stats285_hw3/main.py
