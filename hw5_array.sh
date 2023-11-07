42fef826c500bb26afdaecec57507bf21437ca41
adonoho <github@store.ddg.com>
2023-11-07 15:30:12 -0600

    revert to for loop

--- hw5_array.sh
+++ hw5_array.sh
@@ -9,8 +9,8 @@
 #SBATCH --array=0-999:100
 #SBATCH --nodes 1
 
-#for i in {0..9}
-while [ $((SLURM_ARRAY_TASK_ID)) -le $((SLURM_ARRAY_TASK_MAX)) ]
+#while [ $((SLURM_ARRAY_TASK_ID)) -le $((SLURM_ARRAY_TASK_MAX)) ]
+for i in {0..9}
 do
   srun -n 1 python3 ~/Stats285_F23/map_function.py 1000 1000 $((SLURM_ARRAY_TASK_ID)) su_id_hw5 &
 done
