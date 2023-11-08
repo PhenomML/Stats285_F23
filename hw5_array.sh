09bd95dce5422f2cdbb5ff3e38b04e9587e9e8e0
adonoho <github@store.ddg.com>
2023-11-07 20:08:00 -0600

    Simplify the script.

--- hw5_array.sh
+++ hw5_array.sh
@@ -9,10 +9,13 @@
 #SBATCH --array=0-999:100
 #SBATCH --nodes 1
 
-#for i in {0..9}
-while [ $SLURM_ARRAY_TASK_ID -le $SLURM_ARRAY_TASK_MAX ]
+ml anaconda3/2023.07
+source activate stats285
+
+#while [ $SLURM_ARRAY_TASK_ID -le $SLURM_ARRAY_TASK_MAX ]
+for i in {0..9}
 do
-  srun -n 1 python3 ~/Stats285_F23/map_function.py 1000 1000 $SLURM_ARRAY_TASK_ID su_id_hw5 &
+  srun -n 1 python3 ./map_function.py 1000 1000 $SLURM_ARRAY_TASK_ID su_id_hw5 &
 done
 
 wait # important to make sure the job doesn't exit before the background tasks are done
