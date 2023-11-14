8bc11ca388d13b09ecfcc4289aeb7555850ac58b
David Donoho <sparsity@mac.com>
2023-11-14 03:42:44 -0800

    Update hw5_personalize.sh
    gribbles

--- hw5_personalize.sh
+++ hw5_personalize.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 read -p "Enter your SU Id (Stanford login ID):" SUID
 read -p "Your entry was '$SUID' : Correct SU Id? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-for file in map_function.py main.py ; do
+for file in map_function.py main.py reading_gbq.ipynb ; do
   sed --in-place=${file}.bak -e "s|su_ID|$SUID|g" $file
   echo "Personalization changes made to $file:"
   diff $file.bak $file
