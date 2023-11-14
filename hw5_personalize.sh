b9dbc7ea642a64aa4f95989d6c4c16ccb72bd9f9
xiaoyanh <xiaoyanhn@gmail.com>
2023-11-14 12:52:15 -0800

    remove personalize

--- hw5_personalize.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-read -p "Enter your SU Id (Stanford login ID):" SUID
-read -p "Your entry was '$SUID' : Correct SU Id? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-for file in gather_csv_to_gbq.py reading_gbq.ipynb hw5_dask_cluster.sh HW5_analysis.ipynb ; do
-  sed --in-place=${file}.bak -e "s|su_ID|$SUID|g" $file
-  echo "Personalization changes made to $file:"
-  diff ${file}.bak $file
-done
