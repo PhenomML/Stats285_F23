163e876d89c37e98782f69facc6a00674e98d906
David Donoho <sparsity@mac.com>
2023-11-14 13:17:52 -0800

    Update hw5_dask_cluster.sh

--- hw5_dask_cluster.sh
+++ hw5_dask_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## farmshare deployment file that requests 4 cpu cores for 30 minutes to run main.py
-#SBATCH --job-name=hw5_cluster
+#SBATCH --job-name=hw5_dask_cluster
 #SBATCH --partition=normal
 #SBATCH --cpus-per-task=4
 #SBATCH --time=00:30:00
