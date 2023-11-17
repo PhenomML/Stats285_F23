d34b586151a8dee36d077917a7509a333680a8dc
adonoho <github@store.ddg.com>
2023-11-17 13:43:51 -0600

    push for call.

--- vizier_demo.py
+++ vizier_demo.py
@@ -26,20 +26,31 @@ class EvalOnCluster(object):
         self.client = client
         self.credentials = credentials
         self.experiment = None  # Iterable returning (future, df).
+        self.keys = None
 
-    def eval_params(self, instance: callable, params: dict) -> Future:
+    def eval_params(self, instance: callable, params: dict) -> tuple:
+        """
+        Evaluate the instance with the params and return a tuple of param values that could become a key in a dict.
+        :param instance: The `callable` to be invoked on the cluster.
+        :param params: The `kwargs` to be passed to the `instance`
+        :return: A tuple of param values suitable to become a key in a dict.
+        """
 
+        if self.keys is None:
+            self.keys = sorted(params.keys())
         future = self.client.submit(instance, **params)
         if self.experiment is None:
             self.experiment = as_completed([future], with_results=True)
         else:
             self.experiment.add(future)
+        return tuple(params[k] for k in self.keys)
 
-    def result(self) -> DataFrame:
+    def result(self) -> (DataFrame, tuple):  # Return a DataFrame and a key.
         future, result = next(self.experiment)
         self.db.push(result)
         future.release()  # EP function; release the data; will not be reused.
-        yield result
+        values = result[self.keys].to_numpy()
+        yield result, tuple(v for v in values[0])
 
     def final_push(self):
         self.db.final_push()
@@ -47,8 +58,8 @@ class EvalOnCluster(object):
 
 
 # Objective function to maximize.
-def evaluate(*, w: float, x: int, y: float, z: str) -> DataFrame:
-    objective =  w**2 - y**2 + x * ord(z)
+def experiment(*, w: float, x: int, y: float, z: str) -> DataFrame:
+    objective = w**2 - y**2 + x * ord(z)
     return DataFrame(data={'w': w, 'x': x, 'y': y, 'z': z, 'objective': objective}, index=[0])
 
 
@@ -72,7 +83,7 @@ def setup_vizier():
         suggestions = study.suggest(count=1)
         for suggestion in suggestions:
             params = suggestion.parameters
-            df = evaluate(**params)
+            df = experiment(**params)
             suggestion.complete(vz.Measurement({'metric_name': df.iloc[0]['objective']}))
 
 
@@ -83,13 +94,15 @@ def setup_vizier_on_local_cluster():
         with Client(cluster) as client:
             ec = EvalOnCluster(client, None)
             # ec = EvalOnCluster(client, 'test_cluster_01')
-            for i in range(100):
-                suggestions = study.suggest(count=1)
-                for suggestion in suggestions:
-                    params = suggestion.parameters
-                    _ = ec.eval_params(evaluate, dict(params))
-                    df = next(ec.result())
-                    suggestion.complete(vz.Measurement({'metric_name': df.iloc[0]['objective']}))
+            in_cluster = {}
+            for suggestion in study.suggest(count=1):
+                params = suggestion.parameters
+                key = ec.eval_params(experiment, dict(params))
+                in_cluster[key] = suggestion
+            for df, key in ec.result():
+                suggestion = in_cluster[key]
+                suggestion.complete(vz.Measurement({'metric_name': df.iloc[0]['objective']}))
+                del in_cluster[key]
             ec.final_push()
 
 
